9664075eb76085bf99d7b9f93539e2e049f1c2d7
kombi2012kombi <otabekovkamron5555@gmail.com>
2024-12-26 18:34:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = ["Olma", "Banan", "Gilos", "Shaftoli"];
+console.log(a);
+alert(`Boshlangich Arrayning uzunligi ${a.length}`);
+alert("Lyuboy malumotni optashimiz");
+a.pop();
+console.log(a);
+
+alert(a.length);
